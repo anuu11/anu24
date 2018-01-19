bdb64e434ccd4c6af62a759a0cbec0ea18d10da9
anuu11 <35534904+anuu11@users.noreply.github.com>
2018-01-19 18:20:13 +0530

    Add files via upload

---
+++ anu24.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+ 
+void main()
+{
+  int a[100], min,size,i,j,temp;
+ 
+  printf("Enter the number of elements in array\n");
+  scanf("%d", &size);
+ 
+  printf("Enter %d integers\n", size);
+ 
+  for (i = 0; i < size; i++)
+    scanf("%d", &a[i]);
+ 
+  for (i = 0; i < size; i++)
+  {
+for( j=i+1;j<size;j++)
+{
+    if (a[i]>a[j])
+{
+temp=a[i];
+a[i]=a[j];
+a[j]=temp;
+}
+}
+}
+  printf("elements in ascending order are:");
+for(i=0;i<size;i++)
+{printf("%d",a[i]);
+}
+}
